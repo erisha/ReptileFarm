6648d9d497771aa86aba11ccbe0192875c750707
erisha <forever.erisha@gmail.com>
2022-08-08 08:05:56 -0400

    basic models done

---
+++ app/models/reptile.js
@@ -0,0 +1,59 @@
+// REPTILE -> have an owner, that is a user
+// eventually we'll add an array of snack subdocuments
+
+const mongoose = require('mongoose')
+
+const snackSchema = require('./snack')
+
+const { Schema, model } = mongoose
+
+const reptileSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        type: {
+            type: String,
+            required: true
+        },
+        age: {
+            type: Number,
+            required: true
+        },
+        adoptable: {
+            type: Boolean,
+            required: true
+        },
+       snacks : [snackSchema],
+        owner: {
+			type: Schema.Types.ObjectId,
+			ref: 'User'
+		}
+    }, {
+        timestamps: true,
+        // we're going to be adding virtuals to our model, the following lines will make sure that those virtuals are included whenever we return JSON or an Object
+        toObject: { virtuals: true },
+        toJSON: { virtuals: true }
+    }
+)
+
+// virtuals go here
+// these are virtual properties, that use existing data(saved in the database), to add a property whenever we retieve a document and convert it to JSON or an object.
+reptileSchema.virtual('fullTitle').get(function () {
+    // in here, we can do whatever javascripty things we want, to make sure we return some value that will be assigned to this virtual
+    // fullTitle is going to combine the name and type to build a title
+    return `${this.name} the ${this.type}`
+})
+
+reptileSchema.virtual('isABaby').get(function () {
+    if (this.age < 5) {
+        return "yeah, they're just a baby"
+    } else if (this.age >= 5 && this.age < 10) {
+        return "not really a baby, but still a baby"
+    } else {
+        return "a good old reptile(definitely still a baby)"
+    }
+})
+
+module.exports = model('Reptile', ReptileSchema)
---
+++ app/models/snack.js
@@ -0,0 +1,32 @@
+// import dependencies
+const mongoose = require('mongoose')
+
+// snack is a subdocument NOT A MODEL
+// snack will be part of the snacks array added to specific pets
+
+// we dont, DO NOT, need to get the model from mongoose, so we're going to save a lil real estate in our file and skip destructuring, in favor of the regular syntax
+const snackSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    isAlive: {
+        type: Boolean,
+        default: false,
+        required: true
+    },
+    condition: {
+        type: String,
+        // here we're going to use enum, which means we can only use specific strings to satisfy this field.
+        // enum is a validator on the type String, that says "you can only use one of these values"
+        enum: ['loves', 'likes', 'edible'],
+        default: 'likes'
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = snackSchema
